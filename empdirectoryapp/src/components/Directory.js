2500115df155f5ed1f606956c955e684712ff75f
Keita Suzuki <ksuzuki417@gmail.com>
2020-12-04 04:27:28 -0600

    data json created:

--- empdirectoryapp/src/components/Directory.js
+++ empdirectoryapp/src/components/Directory.js
@@ -1,11 +1,30 @@
 import React from "react";
 
-function Directory() {
+function Directory(props) {
     return(
-        <div>
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Email Address</th>
+                    <th scope="col">Github ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                <td> {props.id}</td>
+                <td> {props.firstName}</td>
+                <td> {props.lastName}</td>
+                <td> {props.title}</td>
+                <td> {props.email}</td>
+                <td> {props.github}</td>
+
+            </tbody>
 
             
-        </div>
+        </table>
     )
 }
 
